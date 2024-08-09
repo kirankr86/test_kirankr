adf0b1b6a7ede167490188d2dced05998e0c1441
kiran Jangam <kirankr6@gmail.com>
2024-08-09 16:10:09 +0530

    terraform additions

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,11 @@
-[200~# Configure the AWS provider
 provider "aws" {
   region = "us-west-2" # Replace with your desired region
 }
+
+provider "azure" {
+  region = "us-east-1" # Replace with your desired region
+}
+
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
@@ -10,5 +14,8 @@ resource "aws_instance" "example" {
 
   tags = {
     Name = "my-first-ec2-instance"
+  }
+  tags = {
+    Name = "my-second-ec2-instance"
   }
 }
